ca1aa92508e13ef1544f136a080895bae4c49cec
Vapaa Eero <eero.hallikainen@gmail.com>
2025-03-19 15:17:09 +0200

    fixes

--- backend/esbuild.config.js
+++ backend/esbuild.config.js
@@ -1,18 +1,22 @@
 const esbuild = require('esbuild')
-const copyPlugin = require('esbuild-plugin-copy').copy
+const { copy } = require('esbuild-plugin-copy')
 
+// Run the esbuild process
 esbuild.build({
-  entryPoints: ['../frontend/src/main.jsx'], 
-  bundle: true,
-  outfile: 'dist/out.js', 
+  entryPoints: ['../frontend/src/main.jsx'],  // Entry point of the frontend
+  bundle: true,  // Bundle all files into one JS file
+  minify: true,  // Minify the code for production
+  outfile: 'dist/out.js',  // Output bundled JS file in the dist folder
   plugins: [
-    copyPlugin({
-      resolveFrom: 'cwd', 
-      assets: {
-        from: ['../frontend/public/*'], 
-        to: ['./dist/public/'], 
-      },
+    copy({
+      resolveFrom: 'cwd',  // Make sure paths are resolved from the current working directory
+      assets: [
+        {
+          from: '../frontend/public/*',  // Copy all files from the public directory
+          to: './dist/public/',  // Place them in the dist/public folder
+        },
+      ],
     }),
   ],
 // eslint-disable-next-line no-undef
-}).catch(() => process.exit(1))
+}).catch(() => process.exit(1))  // Exit if the build fails
