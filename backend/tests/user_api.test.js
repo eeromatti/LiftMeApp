011a50957882f981f2ae8b0283d672114b8d887a
Vapaa Eero <eero.hallikainen@gmail.com>
2025-03-24 19:01:20 +0200

    added token authorization to backend tests

--- backend/tests/user_api.test.js
+++ backend/tests/user_api.test.js
@@ -16,40 +16,80 @@ test('user can be created', async () => {
     .post('/api/users/signup')
     .send(helper.initialUsers[0])
     .expect(201)
-  const response = await api.get('/api/users')
-  const user = response.body[0]
-  assert.strictEqual(user.name, 'Keravan Kata')
 })
+
+test('login', async () => {
+  await api
+    .post('/api/users/login')
+    .send({ 'email': 'keravankata@liftmeapp.com', 'password': 'salainen'})
+    .expect(200)
+})
+
+test('users cannot be fetched without the token', async () => {
+  await api
+    .get('/api/users')
+    .expect(500)
+
+  const response = await api
+    .post('/api/users/login')
+    .send({ 'email': 'keravankata@liftmeapp.com', 'password': 'salainen'})
+    .expect(200)
+  const token = response.body.token
+
+  await api
+    .get('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+    .expect(200)
+})
+
 
 test('new user is created and the previously added is going to be a potential driver', async () => {
   await api
     .post('/api/users/signup')
     .send(helper.initialUsers[1])
     .expect(201)
-  const response = await api.get('/api/users')
-  const user = response.body[1]
+
+  const response1 = await api
+    .post('/api/users/login')
+    .send({ 'email': 'keravankata@liftmeapp.com', 'password': 'salainen'})
+  const token = response1.body.token
+  
+  const response2 = await api
+    .get('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+  const user = response2.body[1]
   assert.strictEqual(user.drivers[0].name, 'Keravan Kata')
 })
 
 test('update matches for the user first added', async () => {
+  // log in to get a token
+  const response1 = await api
+    .post('/api/users/login')
+    .send({ 'email': 'keravankata@liftmeapp.com', 'password': 'salainen'})
+  const token = response1.body.token
+  
   // find the id of the user first added
-  const response = await api.get('/api/users')
-  const id = response.body[0]._id
+  const response2 = await api
+    .get('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+  const id = response2.body[0]._id
 
   // update the matches of that user
-  await api.put(`/api/users/matches/${id}`)
-
-  // find the user
-  const response2 = await api.get('/api/users')
-  const user = response2.body[0]
-  assert.strictEqual(user.passengers[0].name, 'Korson Kartsa')
-})
-
-test('login', async () => {
   await api
-    .post('/api/users/login')
-    .send({ 'email': 'korsonkartsa@liftmeapp.com', 'password': 'salainen'})
-    .expect(200)
+    .put(`/api/users/matches/${id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+    
+  // find the user
+  const response3 = await api
+    .get('/api/users')
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+  const user = response3.body[0]
+  assert.strictEqual(user.passengers[0].name, 'Korson Kartsa')
 })
 
 after(async () => {
