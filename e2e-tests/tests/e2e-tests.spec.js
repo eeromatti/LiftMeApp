daed846cfaeb6b1ce103b3d65a1c127a230fa717
Vapaa Eero <eero.hallikainen@gmail.com>
2025-03-26 19:05:32 +0200

    simplified more

--- e2e-tests/tests/e2e-tests.spec.js
+++ e2e-tests/tests/e2e-tests.spec.js
@@ -9,23 +9,23 @@ describe('LiftMeApp', () => {
         await page.getByRole('textbox').first().fill('eskolaine@liftmeapp.com')
         await page.getByRole('textbox').last().fill(process.env.PASSWORD)
         await page.getByRole('button', { name: 'SIGN IN' }).click();
-        // try {
-        //     await page.waitForSelector('text=Esko Laine', { timeout: 60000 });
-        // } catch (error) {
-        //     console.log(await page.content())
-        //     throw error;
-        // }
-        // await expect(page.getByText('Esko Laine', { timeout: 60000 })).toBeVisible()
+        try {
+            await page.waitForSelector('text=Esko Laine', { timeout: 60000 });
+        } catch (error) {
+            console.log(await page.content())
+            throw error;
+        }
+        await expect(page.getByText('Esko Laine', { timeout: 60000 })).toBeVisible()
     })
     
     test('drivers and passengers available & profile page accessible', async ({ page }) => {
         await expect(page.getByText('Linda Lorén')).toBeVisible()
         await page.getByRole('button', { name: 'passengers' }).click(),
         await expect(page.getByText('Masa Aaltonen')).toBeVisible()
-        await page.getByRole('button', { name: 'Account Menu' }).click()
-        await expect(page.getByText('My profile')).toBeVisible()
-        await page.getByTestId('account-menu-button').click()
-        await page.waitForURL('**/profile')
+        // await page.getByRole('button', { name: 'Account Menu' }).click()
+        // await expect(page.getByText('My profile')).toBeVisible()
+        // await page.getByTestId('account-menu-button').click()
+        // await page.waitForURL('**/profile')
     })
 })
 
